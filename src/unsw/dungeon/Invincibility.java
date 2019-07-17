b371fdb4a2abcfc4a5703df52897fa8c116436fd
Rama Pringgo <r.surjaudaja@student.unsw.edu.au>
2019-07-17 14:14:50 +1000

    minor changes to fix functions

--- src/unsw/dungeon/Invincibility.java
+++ src/unsw/dungeon/Invincibility.java
@@ -6,7 +6,6 @@ public class Invincibility extends Entity {
 	public Invincibility(int x, int y) {
 		super(x, y);
 		duration = 10;
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void reduceDuration() {
