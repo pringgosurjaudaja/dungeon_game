2a8ac2fd3815ceaaee46297905c6bd13922c63cc
Bernadette Setio <bernadettesharon29@gmail.com>
2019-07-30 01:03:36 +1000

    Added BFS for enemyChase() and enemyRunAway()

---
+++ src/unsw/dungeon/EnemyRunAway.java
@@ -0,0 +1,73 @@
+package unsw.dungeon;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EnemyRunAway implements AutoMoveAction {
+
+	@Override
+	public Point autoMove(ArrayList<Point> listCoordinates , Point source , Point dest , int width , int height) {
+		// TODO Auto-generated method stub
+		//destination would be the opposite of where the player is at
+		Point dest2 = new Point(width-dest.x, height-dest.y);
+		return BFS(source, dest2, listCoordinates , width , height);
+	}
+
+	public Point BFS(Point source , Point dest , ArrayList<Point> obstacles , int width , int height){ 
+		
+		int arrX[] = {0, 0, 1, -1};
+		int arrY[] = {-1,1, 0, 0};
+		
+		ArrayList<Point> visited = new ArrayList<Point>();
+		ArrayList<Point> queue = new ArrayList<Point>(); 
+		
+		Map<Point , Point> parent = new HashMap<Point , Point>();
+		
+		queue.add(source); 
+		
+		while (!queue.isEmpty()) {
+			Point current = queue.remove(0);
+			
+			visited.add(current);
+			
+			if (current.equals(dest))break;
+			
+			for(int i = 0 ;i < arrX.length ; i++){
+				Point newPoint = new Point(current.x + arrX[i] , current.y + arrY[i]);
+				
+				if (current.x + arrX[i] < 0 && current.x >= width)
+					continue;
+				else if (current.y + arrY[i] < 0 && current.y + arrY[i] >= height)
+					continue;
+				else if (obstacles.contains(newPoint))
+					continue;
+				else if(visited.contains(newPoint))
+					continue;
+				else{
+					//format : current , parent
+					parent.put(newPoint , current);
+					queue.add(newPoint);
+					
+					
+				}
+			}
+			
+		}
+		
+		//retracking path
+		Point curr = parent.get(dest);
+		Point previous = null;
+		while(curr != null && !curr.equals(source)){
+			previous = curr;
+			curr = parent.get(curr);
+		}
+		
+		if(previous == null)previous = dest;
+		
+		return previous;
+	}
+	
+
+}
